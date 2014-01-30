4ce09497cb94b2537d19e7d6216ac402d54b2c0e
Gabriel Totoliciu <gabriel@totoliciu.ro>
2014-01-30 19:51:17 +0200

    install scripts

--- install_all.sh
+++ install_all.sh
@@ -1,4 +1,8 @@
+./install_xct.sh
 ./install_brew.sh
+./install_compass.sh
+./install_node.sh
+./install_watch.sh
 
 #add these directories if they are not found to the PATH
 #pwd
---
+++ install_node.sh
@@ -0,0 +1,7 @@
+npm uninstall npm -g
+brew uninstall node
+brew install node
+sudo rm -rf /usr/local/lib/dtrace/node.d
+brew link node
+sudo chmod 777 /usr/local/lib/dtrace/node.d
+brew link node
