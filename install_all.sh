7b54d64b8839e42b0fd2bcb74a56d72dbe0b8942
Gabriel Totoliciu <gabriel@totoliciu.ro>
2014-02-11 13:02:49 +0200

    added ssh-copy-id

--- install_all.sh
+++ install_all.sh
@@ -3,6 +3,7 @@
 ./install_compass.sh
 ./install_node.sh
 ./install_watch.sh
+./install_ssh_copy_id.sh
 
 #add these directories if they are not found to the PATH
 #pwd
---
+++ install_ssh_copy_id.sh
@@ -0,0 +1,2 @@
+#taken from https://coderwall.com/p/iv4izw
+curl https://raw.github.com/beautifulcode/ssh-copy-id-for-OSX/master/ssh-copy-id.sh > /usr/local/bin/ssh-copy-id && chmod +x /usr/local/bin/ssh-copy-id
